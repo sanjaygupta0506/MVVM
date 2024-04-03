631cef8ec3185d0ce93dc78e2c4c38369e9b0c38
DELL <DELL@DESKTOP-ON676KV>
2024-04-03 19:50:46 +0530

    Check-in on 03-04-24

---
+++ Main Project/MainApplication/CSharpConceptApp/Delegates/SingleCastDDelegates.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConceptApp.Delegates
+{
+    // Declare Delegate
+    public delegate void SampleDelegate(int a, int b);
+
+    class MathOperations
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Add Result: {0}", a + b);
+        }
+        public void Subtract(int x, int y)
+        {
+            Console.WriteLine("Subtract Result: {0}", x - y);
+        }
+    }
+
+    class SingleCastDDelegates
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Delegate Example****");
+            MathOperations m = new MathOperations();
+            // Instantiate delegate with add method
+            SampleDelegate dlgt = m.Add;
+            dlgt(100, 90);
+            // Instantiate delegate with subtract method
+            dlgt = m.Subtract;
+            dlgt(100, 90);
+            Console.ReadLine();
+        }
+
+    }
+}
